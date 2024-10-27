7f7a55f64580f997dc98c8335a3573dac0d4454b
Nikolay Zakharin <nikolayzaharyin@gmail.com>
2024-10-27 22:13:24 +0300

    introduced folder and js file

---
+++ lesson 1/script.js
@@ -0,0 +1,21 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", updateName);
+
+function updateName() {
+  const name = prompt("Enter a new name");
+  button.textContent = `Player 1: ${name}`;
+}
+
+
+function createParagraph() {
+    const para = document.createElement("p");
+    para.textContent = "You clicked the button!";
+    document.body.appendChild(para);
+  }
+  
+  const buttons = document.querySelectorAll("button");
+  
+  for (const button of buttons) {
+    button.addEventListener("click", createParagraph);
+  }
